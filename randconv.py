0f1882cb8ebf559404249bf15749a31fd894a81a
ai-idt <162801964+ai-idt@users.noreply.github.com>
2024-10-25 14:55:17 +0200

    update

--- randconv.py
+++ randconv.py
@@ -10,12 +10,10 @@ import random
 def randConv_linear(input_shape,n_layers=3,k=3):
     img_in = tf.keras.layers.Input(shape=input_shape)
     img_ = tf.keras.layers.Conv3D(filters=1,kernel_size=k,strides=1,padding="same",kernel_initializer=tf.keras.initializers.HeNormal())(img_in) 
-    for _ in range(1,n_layers-1):
+    for _ in range(1,n_layers):
         img_ = tf.keras.layers.Conv3D(filters=1,kernel_size=k,strides=1,padding="same",kernel_initializer=tf.keras.initializers.HeNormal())(img_)
 
-    img_out = tf.keras.layers.Conv3D(filters=1,kernel_size=k,strides=1,padding="same",kernel_initializer=tf.keras.initializers.HeNormal())(img_)
-
-    return tf.keras.Model(img_in,img_out)
+    return tf.keras.Model(img_in,img_)
 
 #This is an example code to perform random convolution on an input image
 #Note that in our paper, we normalized images into [0;1] range before RC
